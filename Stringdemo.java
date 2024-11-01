392da09d3487ea819be7815998f38117ca864cc7
Nagalakshmi-735 <151905017+Nagalakshmi-735@users.noreply.github.com>
2024-11-01 10:18:44 +0530

    Create Stringdemo.java

---
+++ Stringdemo.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.lang.String;
+public class StringDemo{
+    public static void main(String args[]){
+        String s1 = "welcome";
+        char ch[]={'j','y','o','t','h','i'};
+        String s2=new String(ch);
+        String s3=new String("Example program");
+        System.out.println("The first string is"+s1);
+        System.out.println("The second string is"+s2);
+        System.out.println("The third string is"+s3);
+    }
+}
